0778adb95992548aa913afa0a66cb818ff5922d0
initMayday <initMayday@protonmail.com>
2025-11-22 00:58:18 +0000

    Added DBUS_SESSION_BUS_ADDRESS env var, and added addtional definitions to PATH

--- main.cpp
+++ main.cpp
@@ -31,11 +31,14 @@ std::string get_env_var(const std::string& var) {
 // MUST RUN AS ROOT, GETS ENV VARS FOR SPECIFIED USER
 std::unordered_map<std::string, std::string> get_env_vars(passwd* pw) {
     std::unordered_map<std::string, std::string> env_vars;
-    env_vars.insert({"XDG_RUNTIME_DIR", std::string("/run/user/" + std::to_string(pw->pw_uid))}); // Arbitrary env var we need
-    env_vars.insert({"PATH", "/usr/bin/"}); // Arbitrary, but most people will probably use this. Potentially remove in future.
+    std::string run_dir = std::string("/run/user/" + std::to_string(pw->pw_uid));
+    env_vars.insert({"XDG_RUNTIME_DIR", run_dir}); // Arbitrary env var we need
+    env_vars.insert({"PATH", "/usr/bin:/usr/local/bin:/usr/local/sbin"}); // Arbitrary, but most people will probably use this.
+    // In future, migrate to sourcing /etc/profile - it specifies the PATH exactly
     env_vars.insert({"SHELL", pw->pw_shell});
     env_vars.insert({"HOME", pw->pw_dir});
     env_vars.insert({"LOGNAME", pw->pw_name});
+    env_vars.insert({"DBUS_SESSION_BUS_ADDRESS", run_dir + "/bus"});
 
     return env_vars;
 }
